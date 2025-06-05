96afab389421c38e5fdd5f52e7ef81a7ac88fcad
Denilsson Montoya Neyra <dmontoyane@gmail.com>
2025-06-05 21:05:47 +0200

    Add create User Intertface InvierteIO/tfm#43

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {GalleryModule} from '@ks89/angular-modal-gallery';
 import { tokenInterceptor } from '@core/interceptors/token.interceptor';
